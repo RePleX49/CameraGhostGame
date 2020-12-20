671f866903b0e3110801dafb9497f9ea2b225de5
Alekai McAdam <am8578@nyu.edu>
2020-12-20 01:08:50 -0500

    - added all audio (ui [not implemented], furniture, & ambient) - object audio script with 4 functions: PlayStart, PlayConstant, StopConstant, and PlayEnd - added audio manager prefab, controlled based on insanity

---
+++ Assets/Scripts/Alekai/AudioManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private GameManager _gameManager;
+
+    [SerializeField] private AudioSource breathingSource;
+    [SerializeField] private float maxVolume;
+    [SerializeField] private float minVolume;
+
+    [SerializeField] private float checkStep = .1f;
+
+    void Start()
+    {
+        _gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
+        StartCoroutine(InsanityCheck());
+    }
+    
+    private IEnumerator InsanityCheck()
+    {
+        while (enabled)
+        {
+            Debug.Log("check");
+            float mentalHealth = _gameManager.mentalHealth;
+            
+            if (mentalHealth > 0)
+            {
+                var percent = 1 - (mentalHealth / 100);
+                var range = maxVolume - minVolume;
+
+                breathingSource.volume = minVolume + (percent * range);
+            
+                yield return new WaitForSeconds(checkStep);
+            }
+        }
+    }
+}
