6358b8664eabfcaaff9dda7846461909d92dd4e5
Luke Lin <lukelin2009@hotmail.com>
2021-03-07 17:57:12 -0800

    +Started Prototyping Save System

---
+++ Assets/Collaborators/Luke/Scripts/Save System/SerializationTest.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SerializationTest : MonoBehaviour
+{
+    public interface ISaveable
+    {
+        int Version { get; }
+        string GetString();
+        object SetFromString(string textData);
+        int NumberOfBytes();
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Raymond/PlayerMovement.cs
+++ Assets/Scripts/Raymond/PlayerMovement.cs
@@ -38,10 +38,11 @@ public class PlayerMovement : MonoBehaviour
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
 
-        float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Vertical");
+        float x = Input.GetAxisRaw("Horizontal");
+        float z = Input.GetAxisRaw("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
+        move.Normalize();
 
         controller.Move(move * speed * Time.deltaTime);
 
