b7fb0a04de0f8e597cd0a117a393a20df3923e50
Luke Lin <lukelin2009@hotmail.com>
2021-04-11 12:19:24 -0700

    +Started InputModeManager

---
+++ Assets/Collaborators/Luke/Scripts/Managers/InputModeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputModeManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void SwitchInputModeMenu()
+    {
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void SwitchInputModeGame()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+}
