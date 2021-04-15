3ecd2f23284102d9ec37d4454ac2ac94d8e24c3e
Para!! <pianoducks.paradox@gmail.com>
2021-04-15 18:55:57 -0500

    finished edits to main menu, added cecil's photo

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    private Boolean newGame;
+    public GameObject mainMenu;
+    public GameObject startGame;
+    public Animator cameraAC;
+    public GameObject soundMenu;
+    void Start()
+    {
+        mainMenu.SetActive(true);
+        newGame = false;
+        startGame.SetActive(false);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (newGame)
+            {
+                SceneManager.LoadScene("NewBlockoutREVISE");
+            }
+        }
+    }
+    
+    //button methods
+    
+    //start a new game
+    public void NewGame()
+    {
+        newGame = true;
+        mainMenu.SetActive(false);
+        cameraAC.SetBool("newGameAnim", true);
+        startGame.SetActive(true);
+    }
+
+    //start from last save point
+    public void Retry()
+    {
+        
+    }
+
+    public void OpenOptions()
+    {
+        mainMenu.SetActive(false);
+        soundMenu.SetActive(true);
+    }
+    
+    public void CloseOptions()
+    {
+        mainMenu.SetActive(true);
+        soundMenu.SetActive(false);
+    }
+
+    //exit to main menu
+    public void Exit()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+    
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
